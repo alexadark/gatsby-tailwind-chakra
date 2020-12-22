6b71b5510609da4892911c10ac059f58543cea2e
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-22 18:09:12 +0100

    chakra theme

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,12 +1,12 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react"
 
-import foundations from './foundations'
+import foundations from "./foundations"
 
-import components from './components'
-import layerStyles from './layerStyles'
-import textStyles from './textStyles'
+import components from "./components"
+import layerStyles from "./layerStyles"
+import textStyles from "./textStyles"
 
-import styles from './styles'
+import styles from "./styles"
 
 const theme = extendTheme({
   ...foundations,
