a7cc002e1a2a2f68985ba3befb08f23a0a9e5755
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-22 12:15:11 +0100

    add chakra

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Box } from "@chakra-ui/react"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="py-4 text-center text-primary bg-test rounded-4">
-      Hi people
-    </h1>
+    <Box className="bg-secondary" p={10}>
+      <h1 className="py-4 text-center text-primary bg-test rounded-4">
+        Hi people
+      </h1>
+    </Box>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
